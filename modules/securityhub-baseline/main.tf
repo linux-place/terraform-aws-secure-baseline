862fb35fb34b182bb63934f7bcef17f4bf2e696b
Carlos <carlos.eduardo@linuxplace.com.br>
2019-10-23 20:07:51 -0300

    Modificando securityhub

--- modules/securityhub-baseline/main.tf
+++ modules/securityhub-baseline/main.tf
@@ -2,12 +2,14 @@
 # Enable SecurityHub
 # --------------------------------------------------------------------------------------------------
 resource "aws_securityhub_account" "main" {
+  count = var.enabled ? 1 : 0
 }
 
 # --------------------------------------------------------------------------------------------------
 # Subscribe CIS benchmark
 # --------------------------------------------------------------------------------------------------
 resource "aws_securityhub_standards_subscription" "cis" {
+  count = var.enabled ? 1 : 0
   depends_on    = [aws_securityhub_account.main]
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
 }
