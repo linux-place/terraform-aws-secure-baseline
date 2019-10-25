5f5c63a4867149e29f27593538079932d5173f50
Fernando Augusto Medeiros Silva <fernando.augusto@gmail.com>
2019-10-25 18:29:56 -0300

    Adicionado members e invites para o securityhub

--- modules/securityhub-baseline/main.tf
+++ modules/securityhub-baseline/main.tf
@@ -5,6 +5,23 @@ resource "aws_securityhub_account" "main" {
   count = var.enabled ? 1 : 0
 }
 
+resource "null_resource" "aws_securityhub_members" {
+  count = length(var.member_accounts)
+
+  provisioner "local-exec" {
+    # Bootstrap script called with private_ip of each node in the clutser
+    #command = ${format("aws securityhub create-member --account-details AccountId=%s,Email=%s",${var.member_accounts[count.index].account_id},${var.member_accounts[count.index].email})}
+    command = "aws securityhub create-members --account-details AccountId=${var.member_accounts[count.index].account_id},Email=${var.member_accounts[count.index].email} --region ${data.aws_region.current.name}"
+  }
+}
+resource "null_resource" "aws_securityhub_invitations" {
+  count = length(var.member_accounts)
+  provisioner "local-exec" {
+    # Bootstrap script called with private_ip of each node in the clutser
+    #command = ${format("aws securityhub create-member --account-details AccountId=%s,Email=%s",${var.member_accounts[count.index].account_id},${var.member_accounts[count.index].email})}
+    command = "aws securityhub invite-members --account-ids ${var.member_accounts[count.index].account_id} --region ${data.aws_region.current.name}"
+  }
+}
 # --------------------------------------------------------------------------------------------------
 # Subscribe CIS benchmark
 # --------------------------------------------------------------------------------------------------
@@ -13,4 +30,5 @@ resource "aws_securityhub_standards_subscription" "cis" {
   depends_on    = [aws_securityhub_account.main]
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
 }
+
 
--- modules/securityhub-baseline/variables.tf
+++ modules/securityhub-baseline/variables.tf
@@ -6,3 +6,12 @@ variable "sechub_subs_enabled" {
   description = "The boolean flag whether this module is enabled or not. No resources are created when set to false."
   default     = true
 }
+variable "member_accounts" {
+  description = "A list of IDs and emails of AWS accounts which associated as member accounts."
+  type = list(object({
+    account_id = string
+    email      = string
+  }))
+  default = []
+}
+data "aws_region" "current" {}
